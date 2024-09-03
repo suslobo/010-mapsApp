5c0e110cb12bd168045da538fbf518334b684fa6
slobo <suslobo@hotmail.es>
2024-09-03 12:33:07 +0200

    Menú de la aplicación

--- src/app/maps/components/side-menu/side-menu.component.ts
+++ src/app/maps/components/side-menu/side-menu.component.ts
@@ -1,10 +1,27 @@
 import { Component } from '@angular/core';
 
+
+
+//creamos una interfaz
+interface MenuItem {
+  name: string;
+  route: string;
+}
+
 @Component({
-  selector: 'app-side-menu',
+  selector: 'maps-side-menu',
   templateUrl: './side-menu.component.html',
   styleUrl: './side-menu.component.css'
 })
 export class SideMenuComponent {
+  //Creamos las cuatro rutas, las mismas rutas que pusimos en maps-routing.module.ts
+  public menuItems: MenuItem[] = [
+    { route: '/maps/fullscreen', name: 'FullScreen' },
+    { route: '/maps/zoom-range', name: 'ZoomRange' },
+    { route: '/maps/markers', name: 'Markers' },
+    { route: '/maps/properties', name: 'Houses' },
+    ];
 
 }
+
+
