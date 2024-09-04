75bc7d8ef4e8ad13adceecf0f09052edb20e449d
slobo <suslobo@hotmail.es>
2024-09-04 16:09:31 +0200

    Mostrar un mapa en pantalla completa

--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -1,9 +1,52 @@
-import { Component } from '@angular/core';
+/*
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
 
 @Component({
   templateUrl: './full-screen-page.component.html',
-  styleUrl: './full-screen-page.component.css'
+  styleUrls: ['./full-screen-page.component.css']
 })
-export class FullScreenPageComponent {
+export class FullScreenPageComponent implements AfterViewInit {
 
+  @ViewChild('map') divMap?: ElementRef;
+
+
+  ngAfterViewInit(): void {
+
+    if ( !this.divMap ) throw 'El elemento HTML no fue encontrado';
+
+    const map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
+
+
+
+}
+ */
+import { AfterViewInit, Component } from '@angular/core';
+
+import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
+
+(mapboxgl as any).accessToken =
+  'pk.eyJ1Ijoid2lsZnJlZG9ndWVycmEiLCJhIjoiY2x4ZW9saTZiMGlvODJrcHQwdnZicTVxbSJ9._igcWsi7fTK8nb9bOx_aag';
+
+@Component({
+  templateUrl: './full-screen-page.component.html',
+  styleUrls: ['./full-screen-page.component.css']
+})
+export class FullScreenPageComponent implements AfterViewInit {
+
+  ngAfterViewInit(): void {
+    const map = new mapboxgl.Map({
+      container: 'map', // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: [-74.5, 40], // starting position [lng, lat]
+      zoom: 9, // starting zoom
+    });
+  }
 }
