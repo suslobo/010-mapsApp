167ed17ec31c5f8eaa83124270f040ca7ac9acf5
slobo <suslobo@hotmail.es>
2024-09-04 16:23:40 +0200

    ViewChild - ElementRef

--- src/app/maps/maps.module.ts
+++ src/app/maps/maps.module.ts
@@ -1,6 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+import mapboxgl from 'mapbox-gl'; 
+
+(mapboxgl as any).accessToken =
+  'pk.eyJ1Ijoid2lsZnJlZG9ndWVycmEiLCJhIjoiY2x4ZW9saTZiMGlvODJrcHQwdnZicTVxbSJ9._igcWsi7fTK8nb9bOx_aag';
+
 import { MapsRoutingModule } from './maps-routing.module';
 import { MiniMapComponent } from './components/mini-map/mini-map.component';
 import { SideMenuComponent } from './components/side-menu/side-menu.component';
--- src/app/maps/pages/full-screen-page/full-screen-page.component.ts
+++ src/app/maps/pages/full-screen-page/full-screen-page.component.ts
@@ -28,12 +28,8 @@ export class FullScreenPageComponent implements AfterViewInit {
 
 }
  */
-import { AfterViewInit, Component } from '@angular/core';
-
-import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
-
-(mapboxgl as any).accessToken =
-  'pk.eyJ1Ijoid2lsZnJlZG9ndWVycmEiLCJhIjoiY2x4ZW9saTZiMGlvODJrcHQwdnZicTVxbSJ9._igcWsi7fTK8nb9bOx_aag';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { Map } from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 
 @Component({
   templateUrl: './full-screen-page.component.html',
@@ -41,9 +37,14 @@ import mapboxgl from 'mapbox-gl'; // or "const mapboxgl = require('mapbox-gl');"
 })
 export class FullScreenPageComponent implements AfterViewInit {
 
+  @ViewChild('map') divMap?: ElementRef;
+
   ngAfterViewInit(): void {
-    const map = new mapboxgl.Map({
-      container: 'map', // container ID
+
+    if ( !this.divMap ) throw 'El elemento HTML no fue encontrado';
+
+    const map = new Map({
+      container: this.divMap.nativeElement, // container ID
       style: 'mapbox://styles/mapbox/streets-v12', // style URL
       center: [-74.5, 40], // starting position [lng, lat]
       zoom: 9, // starting zoom
