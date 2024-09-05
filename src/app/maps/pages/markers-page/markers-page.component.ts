d844fc3d8503798e67e01a30de790ce1f3e84f0d
slobo <suslobo@hotmail.es>
2024-09-05 10:44:22 +0200

    Crear Marcadores en el mapa

--- src/app/maps/pages/markers-page/markers-page.component.ts
+++ src/app/maps/pages/markers-page/markers-page.component.ts
@@ -1,9 +1,38 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, ViewChild } from '@angular/core';
+import { LngLat, Map, Marker } from 'mapbox-gl';
 
 @Component({
   templateUrl: './markers-page.component.html',
   styleUrl: './markers-page.component.css'
 })
-export class MarkersPageComponent {
+export class MarkersPageComponent implements AfterViewInit {
 
+  @ViewChild('map') divMap?: ElementRef;
+
+  public map?: Map;
+  public currentLngLat: LngLat = new LngLat(-74.9790288940621, 39.92398834469088);
+
+  ngAfterViewInit(): void {
+
+    if ( !this.divMap ) throw 'El elemento HTML no fue encontrado';
+
+    this.map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.currentLngLat, // starting position [lng, lat]
+      zoom: 13, // starting zoom
+    });
+
+    //creamos un elemento HTML
+    /* const markerHtml = document.createElement('div');
+    markerHtml.innerHTML = 'Fernando Herrera'
+
+    const marker = new Marker({
+      color: 'red',
+      element: markerHtml
+    })
+      .setLngLat( this.currentLngLat )
+      .addTo( this.map );
+ */
+  }
 }
