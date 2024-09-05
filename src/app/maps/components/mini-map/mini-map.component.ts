07be80147f8e1328659ff129c9af1054400bf1ea
slobo <suslobo@hotmail.es>
2024-09-05 14:11:01 +0200

    Componente MiniMap

--- src/app/maps/components/mini-map/mini-map.component.ts
+++ src/app/maps/components/mini-map/mini-map.component.ts
@@ -1,10 +1,37 @@
-import { Component } from '@angular/core';
+import { Component, Input, ViewChild, ElementRef } from '@angular/core';
+import { Map, Marker } from 'mapbox-gl';
 
 @Component({
-  selector: 'app-mini-map',
+  selector: 'map-mini-map',
   templateUrl: './mini-map.component.html',
   styleUrl: './mini-map.component.css'
 })
 export class MiniMapComponent {
+
+  @Input() lngLat?: [number, number];
+  //necesitamos la referencia al elemento mediante @ViewChild
+  //lo copiamos de full-screen-page.component.ts
+  @ViewChild('map') divMap?: ElementRef;
+
+  ngAfterViewInit() {
+
+    //necesitamos el mapa
+    //lo copiamos de full-screen-page.component.ts
+    if ( !this.divMap?.nativeElement ) throw "Map Div not found";
+    if ( !this.lngLat ) throw "LngLat can't be null";
+
+    const map = new Map({
+      container: this.divMap.nativeElement, // container ID
+      style: 'mapbox://styles/mapbox/streets-v12', // style URL
+      center: this.lngLat, // starting position [lng, lat]
+      zoom: 15, // starting zoom
+      interactive: false
+    });
+
+    //marker
+    new Marker()
+      .setLngLat( this.lngLat )
+      .addTo( map )
+  }
 
 }
