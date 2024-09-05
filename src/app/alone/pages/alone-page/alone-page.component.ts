16b03987f00e62bded62a054c2847594fa60776d
slobo <suslobo@hotmail.es>
2024-09-05 15:52:25 +0200

    Standalone como componente reutilizable

--- src/app/alone/pages/alone-page/alone-page.component.ts
+++ src/app/alone/pages/alone-page/alone-page.component.ts
@@ -1,13 +1,15 @@
-import { CommonModule } from '@angular/common';
+
 import { Component } from '@angular/core';
+import { CounterAloneComponent } from "../../components/counter-alone/counter-alone.component";
 
 @Component({
   //como va a ser una pag. podemos obviar el selector porque no lo vamos a ocupar
   // selector: 'app-alone-page',
   standalone: true,
-  // imports: [ CommonModule ],
+  imports: [CounterAloneComponent],
   templateUrl: './alone-page.component.html',
-  styleUrl: './alone-page.component.css'
+  styleUrl: './alone-page.component.css',
+
 })
 export class AlonePageComponent {
 
